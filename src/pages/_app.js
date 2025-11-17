b4c5e93c89731e87b5f7211f4ec0278553367921
janithmidissanayake <dissanayakejw.20@uom.lk>
2025-11-17 18:58:09 +0530

    Fix Netlify config and remove duplicate Navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ThemeContextProvider from '../contexts/theme-context';
 import '../styles/globals.css';
-import Navbar from '../components/navbar/navbar';
 
 const muiTheme = createTheme();
 
@@ -10,7 +9,6 @@ const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={muiTheme}>
       <ThemeContextProvider>
-        <Navbar />
         <Component {...pageProps} />
       </ThemeContextProvider>
     </ThemeProvider>
